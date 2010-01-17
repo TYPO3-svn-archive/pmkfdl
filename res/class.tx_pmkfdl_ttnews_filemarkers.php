a99cc52939b3729ce15593f7cc00ca3d09200448
pmk65 <pmk65@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-01-17 12:44:22 +0000

    TER Sync.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pmkfdl/trunk@28920 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/class.tx_pmkfdl_ttnews_filemarkers.php
+++ res/class.tx_pmkfdl_ttnews_filemarkers.php
@@ -85,4 +85,7 @@ class tx_pmkfdl_ttnews_filemarkers {
 		return $parentMarkerArray;
 	}
 }
+	if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/pmkfdl/res/class.tx_pmkfdl_ttnews_filemarkers.php'])	{
+		include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/pmkfdl/res/class.tx_pmkfdl_ttnews_filemarkers.php']);
+	}
 ?>
